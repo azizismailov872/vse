982c1c672e281773b75c035e1853d40f87b4f55a
azizismailov <azizismailov872872@gmail.com>
2020-11-15 16:44:53 +0600

    Добавил контактную страницу

---
+++ frontend/views/site/developer.php
@@ -0,0 +1,43 @@
+<?php 
+
+use yii\helpers\Url;
+use yii\helpers\Html;
+
+?>
+<h2 class="content-header d-lg-block d-none">Контакты разработчика</h2>
+<div class="row mb-4 mt-3">
+	<div class="col-6">
+		<h2 class="developer-contact-header"><a href="https://www.linkedin.com/in/aziz-ismailov-75b7a11bb/" class="developer-contact-link"  target="_blank">Linkedin</a></h2>
+		<p class="developer-contact-text"><a href="https://www.linkedin.com/in/aziz-ismailov-75b7a11bb/"  target="_blank">Aziz Ismailov</a></p>
+	</div>
+	<div class="col-6">
+		<h2 class="developer-contact-header"><a href="https://www.instagram.com/nemovstud/"  target="_blank" class="developer-contact-link">Instagram</a></h2>
+		<p class="developer-contact-text"><a href="https://www.instagram.com/nemovstud/"  target="_blank">@nemovstud</a></p>
+	</div>
+</div>
+<div class="row mb-4">
+	<div class="col-6">
+		<h2 class="developer-contact-header"><a href="https://t.me/nemmov" target="_blank" class="developer-contact-link">Telegram</a></h2>
+		<p class="developer-contact-text"><a href="https://t.me/nemmov"  target="_blank">@nemmov</a></p>
+	</div>
+	<div class="col-6">
+		<h2 class="developer-contact-header"><a href="mailto:azizismailov872872@gmail.com" class="developer-contact-link">Email</a></h2>
+		<p class="developer-contact-text"><a href="mailto:azizismailov872872@gmail.com">azizismailov872872@gmail.com</a></p>
+	</div>
+</div>
+<div class="row mb-3">
+	<div class="col-12">
+		<a href="https://azizismailov872.github.io/contact-page/" class="developer-page-link btn btn-primary">Перейти на личную страницу</a>
+	</div>
+</div>
+<div class="row">
+	<div class="col-12">
+		<h1 class="about-developer-title">Обо мне</h1>
+	</div>
+	<div class="col-12">
+		<p class="about-developer-desc">
+			Меня зовут Азиз Исмаилов. Я фрилансер и занимаюсь интернет деятельностью. Все же основной моей деятельностью является веб-разработка в которую входят такие области как frontend и backend разработка, верстка сайтов, разработка проектов под ключ, улучшения и доработка существующих проектов, курирование веб проектов а так же помощь заказчикам при составлении плана и технического задания и отдельно хочу выделить веб дизайн.
+		</p>
+		<p class="about-developer-desc mt-3">Помимо всего у меня есть команда подчиненных вместе с которой мы занимаемся такими вещами как: Оформление instagram профилей, коппирайтинг, помощь предпринимателям с интернет бизнесом и бизнесом в социальных сетях, SMM.</p>
+	</div>
+</div>
